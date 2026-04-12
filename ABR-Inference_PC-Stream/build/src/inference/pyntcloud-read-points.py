5b4b582affab17c9baded7c5f02745c95e3b2fbc
waquas <mohammadwaqu@umass.edu>
2026-04-12 13:09:44 -0400

    fix name

--- ABR-Inference_PC-Stream/build/src/inference/pyntcloud-read-points.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pyntcloud import PyntCloud
-input= sys.argv[1]
-
-cloud = PyntCloud.from_file(input)
-#print(cloud.points.head())  # Display first few rows of point data
-print((cloud.points))
-
-cloud.points.to_csv(input+".csv", index=False) #save to csv file
